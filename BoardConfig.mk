52ebf504e4200039c59a267a48025f5e7054f2a8
yasu-hide <5038337+yasu-hide@users.noreply.github.com>
2018-10-14 21:13:53 +0900

    js01lte: DO NOT MERGE selinux set permissive
    Change-Id: I2a7fb31dd980c0f2cfdbe50d6f7d1adf30bd325a

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -28,9 +28,7 @@ TARGET_BOOTLOADER_BOARD_NAME := MSM8974
 # Kernel
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_CMDLINE := console=null androidboot.hardware=qcom user_debug=23 msm_rtb.filter=0x37 zcache.enabled=1 zcache.compressor=lz4
-ifeq ($(RECOVERY_VARIANT),twrp)
 BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
-endif
 BOARD_KERNEL_IMAGE_NAME := zImage
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_SEPARATED_DT := true
